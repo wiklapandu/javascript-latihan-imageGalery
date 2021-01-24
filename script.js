53888f4ef27cefba351ccb341c133081d3ecb329
wwwikla <50991164+wiklapandu@users.noreply.github.com>
2021-01-24 20:14:50 +0700

    Latihan Javascript Image Galery
    untuk mengubah gambar jumbo dengan cara ngeklik salah satu gambar yang
    terdapat di dalam html
    Changes to be committed:
    	-> new file:   css/style.css
    	-> new file:   css/style.css.map
    	-> new file:   img/image1.jpg
    	-> new file:   img/image2.jpg
    	-> new file:   img/image3.jpg
    	-> new file:   img/image4.jpg
    	-> new file:   img/image5.jpg
    	-> new file:   img/image6.jpg
    	-> new file:   img/pink.jpg
    	-> new file:   index.html
    	-> new file:   sass/style.scss
    	-> new file:   script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Cara 1
+
+const jumbo = document.querySelector('.jumbo');
+// const thumbs = document.querySelectorAll('.thumb');
+jumbo.setAttribute('src','img/image1.jpg');
+// thumbs.forEach(thumb => {
+//     thumb.addEventListener('click', function(e){
+//         if(jumbo.attributes.getNamedItem('src')){
+//             jumbo.attributes.removeNamedItem('src');
+//         }
+//         jumbo.setAttribute('src',e.target.getAttribute('src'));
+//     });
+// });
+
+
+// Cara 2
+const thumbs =document.querySelectorAll('.thumb');
+const container = document.querySelector('.container');
+container.addEventListener('click',(e)=>{
+    if(e.target.className == 'thumb'){
+        // console.log(e.target.getAttribute('src'));
+        jumbo.src = e.target.getAttribute('src');
+        jumbo.classList.add('fade');
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        }, 500);
+        thumbs.forEach(function(thumb){
+            if( thumb.classList.contains('active') ){
+                thumb.classList.remove('active');
+            }
+        });
+        e.target.classList.add('active');
+    }
+});

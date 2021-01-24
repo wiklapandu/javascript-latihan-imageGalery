5fab85126ef8fbe5cfeee821d0facab0f60f2d3a
wwwikla <50991164+wiklapandu@users.noreply.github.com>
2021-01-24 21:06:11 +0700

    membuat jumlah klik

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ jumbo.setAttribute('src','img/image1.jpg');
 // Cara 2
 const thumbs =document.querySelectorAll('.thumb');
 const container = document.querySelector('.container');
+const klik = document.querySelector('.klik');
+var jm = 0;
+klik.innerHTML = jm;
 container.addEventListener('click',(e)=>{
     if(e.target.className == 'thumb'){
         // console.log(e.target.getAttribute('src'));
@@ -30,5 +33,10 @@ container.addEventListener('click',(e)=>{
             }
         });
         e.target.classList.add('active');
+        jm+= 1;
+        klik.innerHTML = jm;
+        if(jm == 100){
+            alert('selamat anda sudah mencapai ✨ '+jm+' ✨ kali klik 🎉🎉🎉');
+        }
     }
 });
